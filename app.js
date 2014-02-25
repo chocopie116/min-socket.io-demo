95a9014e7632f60bc5f0c59e0c70edd8fc611665
chocopie116@gmail.com <t.kajinuma@gmail.com>
2014-02-26 01:21:43 +0900

    add all emits event

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = require('http').createServer(handler)
 
 app.listen(process.env.PORT || 3000);
 
+var number = 0;
+
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
             function (err, data) {
@@ -17,3 +19,16 @@ function handler (req, res) {
             });
 }
 
+io.set('log level', 1);
+io.sockets.on('connection', function (socket) {
+    //接続段階で現在の状態を通知
+    io.sockets.emit('all_click', number);
+
+    //クライアントからの通知を待ち受けている
+    socket.on('from_client', function () {
+        number++;
+        io.sockets.emit('all_clicks', number); //全員に送信
+        socket.emit('your_click', 1); //自分(クリックした人)のみに送信
+        socket.broadcast.emit('other_click', 1); //自分(クリックした人)以外に送信
+    });
+});
